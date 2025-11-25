fb737c0252d3a60515430b14ce43eb51f9e66ef8
katherinezhu666999-coder <katherinezhu666999@gmail.com>
2025-11-25 17:17:17 +0800

    Create chat.js

---
+++ config/utils/middleware/controllers/routes/chat.js
@@ -0,0 +1,8 @@
+// routes/chat.js
+const express = require('express');
+const router = express.Router();
+const { chat } = require('../controllers/chatController');
+
+router.post('/chat', chat);
+
+module.exports = router;
